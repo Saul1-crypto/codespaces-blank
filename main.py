228f511010ab6e61ba562832724f8f74dd5af3d3
Saul1-crypto <saul.rodriguez@genstudents.org>
2024-11-26 15:35:16 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+nombre = input ("Ingresa tu nombre")
+apellido = input ("Ingresa tu apellido")
+ciudad = input ("Ingresa tu ciudad")
+
+print (f"Hola, {nombre} {apellido} Bienvenido a la plataforma")
